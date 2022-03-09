069d05be823521e2bdbb3b463d46771cd8f293a0
Christian <werbegrafik@rc-design.at>
2022-03-09 09:27:55 +0100

    AddressControler ad Class

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,36 @@
 <?php
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tt_address']['extender'][\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class]['ext_ttaddress']
-    = 'EXT:ifbbaustria_athletendaten/Classes/Domain/Model/Address.php';
+    = 'EXT:athletendaten/Classes/Domain/Model/Address.php';
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'Rcaddressfield.Athletendaten',
         'List',
         [
-            'Address' => 'list',
+            \Rcaddressfield\Athletendaten\Controller\AddressController::class => [ 
+                'address' => 'list'
+            ]
+            //\Rcaddressfield\Athletendaten\Controller\AddressController::class,
+            //[ 
+            //    'Address' => 'list'
+            //]
+            //\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class => 'address,list'
+            //\Rcaddressfield\Athletendaten\Controller\AddressController::class => [ 
+            //    'Address' => 'list'
+            //]
+            //\FriendsOfTYPO3\TtAddress\Controller\AddressController::class => 'Address,list'
+            //'Address' => 'list',
         ],
         // non-cacheable actions
         [
-            'Address' => 'list',
+            \Rcaddressfield\Athletendaten\Controller\AddressController::class => [ 
+                'address' => 'list'
+            ]
+            //\FriendsOfTYPO3\TtAddress\Domain\Model\Address::class => 'address,list'
+            //\Rcaddressfield\Athletendaten\Controller\AddressController::class => [ 
+            //    'Address' => 'list'
+            //]
+            //\FriendsOfTYPO3\TtAddress\Controller\AddressController::class => 'Address,list'
+            //'Address' => 'list',
         ]
     );
